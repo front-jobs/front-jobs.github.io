e3cfdf9027ab9e21ad8b089225730c5a034e2052
Pedro Araujo <pedrotcaraujo@gmail.com>
2016-09-22 00:24:32 -0300

    button style

--- src/components/Greetings/index.jsx
+++ src/components/Greetings/index.jsx
@@ -11,11 +11,15 @@ class Greetings extends React.Component {
 
         return(
             <div className={style.root}>
-                <h1 className={style.logo}>
+                <h1>
                     <img className={style.image} src={src}/>
                     <span className={style.text}>Front Jobs</span>
                 </h1>
-                <p>Procurando front-end?</p>
+                <div>
+                    <h2>Procurando front-end?</h2>
+                    <p>Se você ou sua empresa está precisando de um front-end, anuncie sua vaga no Front Jobs.</p>
+                    <a className={style.button} href="">Anuncie uma vaga</a>
+                </div>
             </div>
         )
     }
