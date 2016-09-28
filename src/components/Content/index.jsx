916a68425aa30f1166b3bd3b9a056c495b779d19
Pedro Araujo <pedrotcaraujo@gmail.com>
2016-09-28 02:52:01 -0300

    firebase setup hello world

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import Firebase from 'firebase';
 import style from './style.css';
 
 class Content extends React.Component {
     constructor() {
         super();
+        this.ref = Firebase.database().ref("greetings");
+        this.ref.on("value", (dataSnapshot) => {
+            this.greetings = dataSnapshot.val();
+            this.setState({
+                greetings: this.greetings
+            })
+        });
     }
 
     render() {
-
         return(
             <div className={style.root}>
-                Hello World!
+                {this.greetings}
             </div>
         )
     }
