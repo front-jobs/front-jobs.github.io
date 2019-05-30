06982c3322898cedbd94947757857fb559dd267f
Daniel Castro <daniel.castro@concrete.com.br>
2019-05-30 16:41:53 -0300

    add new files

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react'
+
+const App = () => (
+  <div className='app'>
+    teste
+  </div>
+)
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
-var path = require('path');
+'use strict'
 
-module.exports = {
-    debug: true,
-    devtool: "eval-source-map",
-    entry: './src/main.jsx',
-    resolve: {
-        modulesDirectories: [path.resolve(__dirname, 'src'), 'node_modules'],
-        extensions: ['', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        publicPath: 'public/',
-        filename: 'main.bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                loaders: [
-                    'style?sourceMap',
-                    'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file'
-            }
-        ]
-    }
-};
+const path = require('path')
+const webpack = require('webpack')
+const validate = require('webpack-validator')
+
+module.exports = validate({
+  devtool: 'source-map',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src', 'index')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'standard'
+    }],
+
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'babel'
+    }]
+  }
+})
